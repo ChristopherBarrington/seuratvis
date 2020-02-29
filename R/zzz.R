6eb38ca24f9417f387493173f01e25707bddceb2
ChristopherBarrington <christopher.barrington@crick.ac.uk>
2020-02-29 14:02:34 +0000

    Create zzz.R
    added defaults for spinners

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname) {
+
+	# shinycssloaders::withSpinner default options
+  options(spinner.type=5,
+          spinner.color='#3498DB',
+          spinner.size=1)
+
+}
