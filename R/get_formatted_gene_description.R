5a52d8303add4af3f4f22c687b095a5ec40480fb
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-04-23 14:23:03 +0100

    Update get_formatted_gene_description.R
    tidyverse-ing and error checking for when biomart cannot be queried

--- R/get_formatted_gene_description.R
+++ R/get_formatted_gene_description.R
@@ -9,10 +9,11 @@
 #' @return a character string of the \code{attribute} from the \code{mart} for the \code{external_gene_name}.
 #' 
 get_formatted_gene_description <- function(external_gene_name, mart) {
-  description <- getBM(mart=mart, attributes='description', filter='external_gene_name', values=external_gene_name[1])
+  tryCatch(expr=getBM(mart=mart, attributes='description', filter='external_gene_name', values=external_gene_name[1]),
+           error=function(x) data.frame(description='Could not query biomaRt!')) -> description 
   
   if(nrow(description)==0) # no description found
-    return('Selected gene')
+    return('No description of chosen feature found in biomaRt')
   
-  str_remove_all(string=description, pattern=' \\[Source.+$') %>% deframe() %>% str_trunc(width=100)
+  description %>% unlist() %>% str_remove_all(string=, pattern=' \\[Source.+$') %>% str_trunc(width=100)
 }
