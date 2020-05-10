1a144ca69e6046d6ac26fbc0b4c4ffb8e064a6df
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-05-10 11:03:53 +0100

    Update zzz.R
    these don't seem to work

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
 
 	# shinycssloaders::withSpinner default options
-  options(spinner.type=5,
-          spinner.color='#3498DB',
-          spinner.size=1)
+  # options(spinner.type=5,
+  #         spinner.color='#3498DB',
+  #         spinner.size=1)
 }
