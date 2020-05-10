fb043b13b3fe0c5c17d0aa2fd714d24e00dd286a
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-05-10 15:15:08 +0100

    updated genes per cell text box module

--- R/text_box.number_of_genes_per_cell.R
+++ R/text_box.number_of_genes_per_cell.R
@@ -4,7 +4,7 @@
 #' 
 #' @param id unique name of the element
 #' @param width integer value for width (rows sum to 12)
-#' @param input,oputput,session used internally
+#' @param input,output,session used internally
 #' @param subtitle type of subtitle to display; one of: \code{default}
 #' @param f,f_name summary function taking one vector and returning a value and a name used to make the subtitle
 #' 
@@ -16,7 +16,7 @@
 #' 
 #' @rdname number_of_genes_per_cell_text_box
 #' 
-number_of_genes_per_cell_text_box.ui <- function(id, width=12, subtitle='default', f=median, f_name=deparse(substitute(f))) {
+number_of_genes_per_cell_text_box.ui <- function(id, width=12, f=median, f_name=deparse(substitute(f))) {
   module <- 'number_of_genes_per_cell'
 
   # make unique id for this object
@@ -25,7 +25,7 @@ number_of_genes_per_cell_text_box.ui <- function(id, width=12, subtitle='default
 
   # create an environment in the seuratvis namespace
   e <- new.env()
-  e$subtitle <- subtitle
+  e$id <- id
   e$summary_function <- f
   e$summary_function_name <- f_name
   assign(x=module_env, val=e, envir=parent.frame(n=1))
@@ -42,11 +42,17 @@ number_of_genes_per_cell_text_box.server <- function(input, output, session) {
 
   # make the text box
   renderValueBox(expr={
+    # get summarised values
+    n_reference <- seuratvis_env$seurat_object.reactions$seurat$nFeature_RNA %>% module_env$summary_function()
+    n_filtered <- seuratvis_env$cell_filtering_data.reactions$filtered_cell_set$nFeature_RNA %>% module_env$summary_function()
+
+    # get the box subtitle
     switch(module_env$id,
-           cell_filtering=sprintf(fmt='%s genes per cell (%s)', str_to_title(module_env$summary_function_name), comma(seuratvis_env$cell_filtering_data.reactions$median_genes_per_cell-seuratvis_env$seurat_object.reactions$reference_metrics$median_genes_per_cell) %>% ifelse(str_detect(., '^-'), ., str_c('+', .))),
+           cell_filtering=sprintf(fmt='%s genes per cell (%s)', str_to_title(module_env$summary_function_name), comma(n_filtered-n_reference) %>% ifelse(str_detect(., '^-'), ., str_c('+', .))),
            sprintf('%s genes per cell', str_to_title(module_env$summary_function_name))) -> subtitle
 
-    list(value={seuratvis_env$cell_filtering_data.reactions$filtered_cell_set$nFeature_RNA %>% module_env$summary_function() %>% round(digits=1) %>% comma()},
+    # create output object
+    list(value={n_filtered %>% round(digits=1) %>% comma()},
          subtitle=subtitle,
          icon=icon('crow')) %>%
       modifyList(x=seuratvis:::text_box_defaults()) %>%
