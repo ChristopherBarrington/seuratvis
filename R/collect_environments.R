0e77e6887ad44653402a1b91a5f77fea174be5b2
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-05-17 11:46:22 +0100

    Update collect_environments.R
    defines module, server and seuratvis global environments in the parent frame

--- R/collect_environments.R
+++ R/collect_environments.R
@@ -3,23 +3,18 @@
 #' Creates environments for module-specific and seuratvis-wide variables
 #' 
 #' @param module name of the module used to determine module-specific environment to load
+#' @param id name of the UI element
 #' 
 #' @return none
 #' 
 #' @details assigns a \code{seuratvis_env} and \code{module_env} environment in the calling function, including copying the \code{id} variable that is passed to \code{callModule()}
 #' 
-collect_environments <- function(module, n=3) {
-  # get the environment using the shiny object identifiers (namespace and id)
-  callmodule_env <- parent.frame(n=2)
-  callmodule_env %>%
-    pluck('id') %>%
-    NS(id=module) %>%
-    sprintf(fmt='%s.env') %>%
-    getFromNamespace(ns='seuratvis') -> module_env
+collect_environments <- function(module, id) {
+  NS(namespace=id, id=module) %>% get(envir=module_environments) -> module_env
 
-  # assign environments back to the calling server function
-  assign(x='seuratvis_env', value=parent.frame(n=n), envir=parent.frame(n=1))
+  assign(x='seuratvis_env', value=parent.env(environment()), envir=parent.frame(n=1))
   assign(x='module_env', value=module_env, envir=parent.frame(n=1))
+  assign(x='server_env', value=parent.frame(n=3), envir=parent.frame(n=1))
 
   invisible('done')
 }
