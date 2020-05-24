9dc8ffd0289385ea0948a4712f9093b862b8bbd9
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-05-24 17:09:26 +0100

    Update collect_environments.R
    checks that the module has an environment to get first

--- R/collect_environments.R
+++ R/collect_environments.R
@@ -10,11 +10,17 @@
 #' @details assigns a \code{seuratvis_env} and \code{module_env} environment in the calling function, including copying the \code{id} variable that is passed to \code{callModule()}
 #' 
 collect_environments <- function(module, id) {
-  NS(namespace=id, id=module) %>% get(envir=module_environments) -> module_env
+  # if module has an environment in module_environments, load it into the parent frame
+  module_env <- NS(namespace=id, id=module)
+  if(exists(x=module_env, envir=module_environments)) {
+    module_env %<>% get(envir=module_environments)
+    assign(x='seuratvis_env', value=parent.env(environment()), envir=parent.frame(n=1))
+  }
 
-  assign(x='seuratvis_env', value=parent.env(environment()), envir=parent.frame(n=1))
+  # load the module and server environments
   assign(x='module_env', value=module_env, envir=parent.frame(n=1))
   assign(x='server_env', value=parent.frame(n=3), envir=parent.frame(n=1))
 
+  # return nothing
   invisible('done')
 }
