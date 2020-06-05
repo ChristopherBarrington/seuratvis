548be4ed2d73b68b51e90bad4d9972594fa9d90d
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-05 15:31:07 +0100

    Update ui.R
    tidying

--- R/ui.R
+++ R/ui.R
@@ -58,39 +58,24 @@ shinyAppUI <- function(...) {
   ### define layout boxes
   gene_highlighting.boxes <- list(cluster_dim_plot=box(title='Clustered map',
                                                        footer='Map showing cell clusters',
-                                                       status='primary',
-                                                       solidHeader=TRUE,
-                                                       width=4,
-                                                       collapsible=TRUE,
+                                                       status='primary', solidHeader=TRUE, width=4, collapsible=TRUE,
                                                        reduced_dimension_plot.ui(id=tab, feature='selected_cluster_resolution')),
                                   gene_expression_map=box(title='Feature on the map',
                                                           footer='Cells coloured by selected feature',
-                                                          status='primary',
-                                                          solidHeader=TRUE,
-                                                          width=4,
-                                                          collapsible=TRUE,
+                                                          status='primary', solidHeader=TRUE, width=4, collapsible=TRUE,
                                                           reduced_dimension_plot.ui(id=tab, feature='picked_feature_values')),
                                   expression_per_cluster=box(title='Feature in clusters',
                                                              footer='Feature values in clusters',
-                                                             status='primary',
-                                                             solidHeader=TRUE,
-                                                             width=4,
-                                                             collapsible=TRUE,
+                                                             status='primary', solidHeader=TRUE, width=4, collapsible=TRUE,
                                                              feature_values_per_cluster_plot.ui(id=tab)),
                                   gene_selector=box(title='Select feature',
-                                                    status='success',
-                                                    solidHeader=TRUE,
-                                                    width=4,
-                                                    collapsible=TRUE,
+                                                    status='success', solidHeader=TRUE, width=4, collapsible=TRUE,
                                                     feature_picker.ui(id=tab, include_metadata_switch=TRUE),
                                                     cluster_resolution_picker.ui(id=tab, include_label_switch=TRUE),
                                                     reduction_method_picker.ui(id=tab),
                                                     assay_picker.ui(id=tab)),
                                   plot_options=box(title='Plot options',
-                                                   status='success',
-                                                   solidHeader=TRUE,
-                                                   width=4,
-                                                   collapsible=TRUE,
+                                                   status='success', solidHeader=TRUE, width=4, collapsible=TRUE,
                                                    colour_palette.ui(id=tab, include_full=TRUE,
                                                                      selectors=list(list(inputId='low', label='Low', value='linen'),
                                                                                     list(inputId='high', label='High', value='darkviolet'))),
@@ -114,69 +99,46 @@ shinyAppUI <- function(...) {
                                        fluidRow(gene_highlighting.boxes$gene_selector,
                                                 gene_highlighting.boxes$plot_options))
 
-
-
-
-
-
   ## gene highlighting on map tab
-  tab <- 'more_gene_highlighting'
-  more_gene_highlighting.tab <- menuItem(text='Highlight features & clusters', tabName=NS(namespace=tab, id='tab'), icon=icon('search'))
+  tab <- 'highlight_features_clusters'
+  highlight_features_clusters.tab <- menuItem(text='Highlight features & clusters', tabName=NS(namespace=tab, id='tab'), icon=icon('search'))
 
   ### define ui elements
 
   ### define layout boxes
-  more_gene_highlighting.boxes <- list(cluster_dim_plot=box(title='Clustered map',
-                                                             footer='Map showing cell clusters',
-                                                             status='primary',
-                                                             solidHeader=TRUE,
-                                                             width=3,
-                                                             collapsible=TRUE,
-                                                             reduced_dimension_plot.ui(id=tab, feature='selected_cluster_resolution')),
-                                       sel_cluster_dim_plot=box(title='Clustered map',
-                                                                footer='Map showing selected cell clusters',
-                                                                status='primary',
-                                                                solidHeader=TRUE,
-                                                                width=3,
-                                                                collapsible=TRUE,
-                                                                reduced_dimension_plot.ui(id=tab, feature='selected_cluster_ids')),
-                                        gene_expression_map=box(title='Feature on the map',
-                                                                footer='Cells coloured by selected feature',
-                                                                status='primary',
-                                                                solidHeader=TRUE,
-                                                                width=3,
-                                                                collapsible=TRUE,
-                                                                reduced_dimension_plot.ui(id=tab, feature='picked_feature_values')),
-                                        expression_per_cluster=box(title='Feature in clusters',
-                                                                   footer='Feature values in clusters',
-                                                                   status='primary',
-                                                                   solidHeader=TRUE,
-                                                                   width=3,
-                                                                   collapsible=TRUE,
-                                                                   feature_values_per_cluster_plot.ui(id=tab)),
-                                        gene_selector=box(title='Select feature',
-                                                          status='success',
-                                                          solidHeader=TRUE,
-                                                          width=4,
-                                                          collapsible=TRUE,
-                                                          feature_picker.ui(id=tab, include_metadata_switch=TRUE),
-                                                          cluster_resolution_picker.ui(id=tab, include_label_switch=TRUE),
-                                                          cluster_id_picker.ui(id=tab),
-                                                          reduction_method_picker.ui(id=tab),
-                                                          assay_picker.ui(id=tab)),
-                                        plot_options=box(title='Plot options',
-                                                         status='success',
-                                                         solidHeader=TRUE,
-                                                         width=4,
-                                                         collapsible=TRUE,
-                                                         colour_palette.ui(id=tab, include_full=TRUE,
-                                                                           selectors=list(list(inputId='low', label='Low', value='linen'),
-                                                                                          list(inputId='high', label='High', value='darkviolet'))),
-                                                         point_size_slider.ui(id=tab),
-                                                         opacity_slider.ui(id=tab)))
+  highlight_features_clusters.boxes <- list(cluster_dim_plot=box(title='Clustered map',
+                                                                  footer='Map showing cell clusters',
+                                                                  status='primary', solidHeader=TRUE, width=3, collapsible=TRUE,
+                                                                  reduced_dimension_plot.ui(id=tab, feature='selected_cluster_resolution')),
+                                            sel_cluster_dim_plot=box(title='Clustered map',
+                                                                     footer='Map showing selected cell clusters',
+                                                                     status='primary', solidHeader=TRUE, width=3, collapsible=TRUE,
+                                                                     reduced_dimension_plot.ui(id=tab, feature='selected_cluster_ids')),
+                                             gene_expression_map=box(title='Feature on the map',
+                                                                     footer='Cells coloured by selected feature',
+                                                                     status='primary', solidHeader=TRUE, width=3, collapsible=TRUE,
+                                                                     reduced_dimension_plot.ui(id=tab, feature='picked_feature_values')),
+                                             expression_per_cluster=box(title='Feature in clusters',
+                                                                        footer='Feature values in clusters',
+                                                                        status='primary', solidHeader=TRUE, width=3, collapsible=TRUE,
+                                                                        feature_values_per_cluster_plot.ui(id=tab)),
+                                             gene_selector=box(title='Select feature',
+                                                               status='success', solidHeader=TRUE, width=4, collapsible=TRUE,
+                                                               feature_picker.ui(id=tab, include_metadata_switch=TRUE),
+                                                               cluster_resolution_picker.ui(id=tab, include_label_switch=TRUE),
+                                                               cluster_id_picker.ui(id=tab),
+                                                               reduction_method_picker.ui(id=tab),
+                                                               assay_picker.ui(id=tab)),
+                                             plot_options=box(title='Plot options',
+                                                              status='success', solidHeader=TRUE, width=4, collapsible=TRUE,
+                                                              colour_palette.ui(id=tab, include_full=TRUE,
+                                                                                selectors=list(list(inputId='low', label='Low', value='linen'),
+                                                                                               list(inputId='high', label='High', value='darkviolet'))),
+                                                              point_size_slider.ui(id=tab),
+                                                              opacity_slider.ui(id=tab)))
 
   ### assemble tab content
-  more_gene_highlighting.content <- tabItem(tabName=NS(namespace=tab, id='tab'),
+  highlight_features_clusters.content <- tabItem(tabName=NS(namespace=tab, id='tab'),
                                        h1('Highlight cell features on the map'),
                                        fluidRow(project_name_text_box.ui(id=tab, width=5),
                                                 picked_feature_and_description_text_box.ui(id=tab, width=7)),
@@ -186,17 +148,12 @@ shinyAppUI <- function(...) {
                                                 number_of_genes_in_assay_text_box.ui(id=tab, width=2),
                                                 number_of_reads_per_cell_text_box.ui(id=tab, width=2, f=median),
                                                 number_of_genes_per_cell_text_box.ui(id=tab, width=2, f=median)),
-                                       fluidRow(more_gene_highlighting.boxes$cluster_dim_plot,
-                                                more_gene_highlighting.boxes$sel_cluster_dim_plot,
-                                                more_gene_highlighting.boxes$gene_expression_map,
-                                                more_gene_highlighting.boxes$expression_per_cluster),
-                                       fluidRow(more_gene_highlighting.boxes$gene_selector,
-                                                more_gene_highlighting.boxes$plot_options))
-
-
-
-
-
+                                       fluidRow(highlight_features_clusters.boxes$cluster_dim_plot,
+                                                highlight_features_clusters.boxes$sel_cluster_dim_plot,
+                                                highlight_features_clusters.boxes$gene_expression_map,
+                                                highlight_features_clusters.boxes$expression_per_cluster),
+                                       fluidRow(highlight_features_clusters.boxes$gene_selector,
+                                                highlight_features_clusters.boxes$plot_options))
 
   ## submit/configure data tab
   submit_data.tab <- menuItem(text='Configure', tabName='submit_data_tab', icon=icon('cogs'), badgeLabel='!', badgeColor='yellow', selected=TRUE)
@@ -220,7 +177,7 @@ shinyAppUI <- function(...) {
   css <- 'table.dataTable tr.active td, table.dataTable td.active {background-color: #3C8DBC !important;}'
   list(cell_filtering.content,
        gene_highlighting.content,
-       more_gene_highlighting.content,
+       highlight_features_clusters.content,
        load_dataset.content) %>%
     do.call(what=tabItems) %>%
     dashboardBody(rclipboardSetup(), tags$head(tags$style(HTML(css)))) -> dashboard_body
@@ -228,7 +185,7 @@ shinyAppUI <- function(...) {
   # sidebar definition
   list(cell_filtering.tab,
        gene_highlighting.tab,
-       more_gene_highlighting.tab,
+       highlight_features_clusters.tab,
        submit_data.tab,
        hr(),
        email_me.tab,
