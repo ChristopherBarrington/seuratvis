62c09a91bfedd86db6d62725fbfce88458a624cc
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-05 17:03:24 +0100

    Update ui_tab.sidebar_links.R
    fixed assignment

--- R/ui_tab.sidebar_links.R
+++ R/ui_tab.sidebar_links.R
@@ -7,5 +7,5 @@
          bug_report=menuItem(text='report a bug', href='mailto:christopher.barrington@crick.ac.uk?subject=[seuratvis] I found a bug!', icon=icon('bug'), newtab=FALSE),
          feature_request=menuItem(text='suggest a feature', href='mailto:christopher.barrington@crick.ac.uk?subject=[seuratvis] It would be cool if...', icon=icon('lightbulb'), newtab=FALSE),
          github_link=menuItem(text=sprintf('GitHub (version: %s)', packageVersion('seuratvis')), href='github.com/ChristopherBarrington/seuratvis', icon=icon('code-branch'), newtab=FALSE)) %>%
-      append(x=menus)})
+      append(x=menus) -> menus})
 }
