8c1f222d7f03a0da3749b73ec664820c10722497
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-13 16:31:57 +0100

    Update preferred_choice.R
    now stops if there is an empty input x vector

--- R/preferred_choice.R
+++ R/preferred_choice.R
@@ -4,11 +4,21 @@
 #' 
 #' @param x vector from which to choose
 #' @param preferences ranked vector of terms to serach for in \code{x}
-#' @param default if no \code{preferences} are found, the \code{default} element of \code{x} is returned
+#' @param default if no \code{preferences} are found, the \code{default}-th element of \code{x} is returned
 #' 
 #' @return an element of \code{x}
 #' 
 preferred_choice <- function(x, preferences, default=1) {
-  preferences %<>% append(pluck(x, default))  
-  x %>% str_c(collapse='|') %>% str_subset(string=preferences) %>% head(n=1)
+  if(length(x)==0)
+    stop('no elements from which to choose!')
+  
+  # if no `preferences` are in `x`, use the first element of `x`
+  preferences %<>% append(pluck(x, default))
+
+  # return the first match in ranked `preferences` that is in `x`
+  x %>%
+    str_c(collapse='|') %>%
+    sprintf(fmt='^(%s)$') %>%
+    str_subset(string=preferences) %>%
+    head(n=1)
 }
