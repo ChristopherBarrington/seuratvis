015b1189818cf26507147f3acaf1d186fe5315a8
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-14 17:24:21 +0100

    Update message.R
    added options() call for verbose messaging

--- R/message.R
+++ R/message.R
@@ -1,7 +1,7 @@
 #' Handle messages to the console
 #' 
 message <- function(...) {
-  if(Sys.getenv('USER')=='chris') 
+  if(getOption('seuratvis.verbose', FALSE))
     do.call(base::message, list(...))
   return()
 }
