c761eedf1d8b4305d11423db54a47b850b653955
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-15 08:50:23 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,1 @@
-.onLoad <- function(libname, pkgname) {
-
-	# shinycssloaders::withSpinner default options
-  # options(spinner.type=5,
-  #         spinner.color='#3498DB',
-  #         spinner.size=1)
-}
+.onLoad <- function(libname, pkgname) {}
