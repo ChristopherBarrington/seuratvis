0aaee6daa3affcccfbcc5ed5606cb84762bd3ebb
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-15 18:29:02 +0100

    Update gui.feature_selector.R
    radio buttons to select feature type, can be updated in a server to restrict set

--- R/gui.feature_selector.R
+++ R/gui.feature_selector.R
@@ -15,7 +15,7 @@
 #' 
 #' @rdname feature_picker
 #' 
-feature_picker.ui <- function(id, label='Feature selection', include_metadata_switch=TRUE) {
+feature_picker.ui <- function(id, label='Feature selection', include_features=TRUE, include_metadata=TRUE, include_values_range=TRUE, features_regex='.*', metadata_regex='.*') {
   sprintf(fmt='### %s-feature_picker.ui', id) %>% message()
 
   module <- 'feature_picker'
@@ -27,37 +27,48 @@ feature_picker.ui <- function(id, label='Feature selection', include_metadata_sw
   # create an environment in the seuratvis namespace
   e <- new.env()
   e$id <- id
-  e$include_metadata_switch <- include_metadata_switch
+  e$include_values_range <- include_values_range
+  e$features_regex <- features_regex
+  e$metadata_regex <- metadata_regex
   assign(x=module_ns, val=e, envir=module_environments)
 
   # record the server(s) to call
   get0(env=module_servers_to_call, x=id) %>% append(sprintf(fmt='%s.server', module)) %>% assign(env=module_servers_to_call, x=id)
 
   # make ui elements
-  ## a metadata switch
-  materialSwitch(inputId=ns(id='list_metadata'), label='Show metadata', value=FALSE, right=FALSE, status='success') -> metadata_switch
-
   ## feature names autocomplete box
-  autocomplete_input(id=ns(id='feature_picker_feature_names'), label='Feature name', options=NULL, value=NULL, placeholder='Feature') %>%
-    conditionalPanel(condition=sprintf('!input["%s"]', ns(id='list_metadata'))) -> feature_names_picker
+  autocomplete_input(id=ns(id='feature_picker_feature_names'), label=NULL, options=NULL, value=NULL, placeholder='Feature') %>%
+    conditionalPanel(condition=sprintf('input["%s"]=="features"', ns(id='feature_type'))) -> feature_names_picker_conditional
 
   ## metadata names drop down box
-  selectizeInput(inputId=ns(id='feature_picker_metadata'), label='Metadata', choices=NULL, selected=NULL) %>%
-    conditionalPanel(condition=sprintf('input["%s"]', ns(id='list_metadata'))) -> metadata_picker
+  selectizeInput(inputId=ns(id='feature_picker_metadata'), label=NULL, choices=NULL, selected=NULL, multiple=FALSE) %>%
+    conditionalPanel(condition=sprintf('input["%s"]=="metadata"', ns(id='feature_type'))) -> metadata_picker_conditional
+
+  ## gene modules drop down box
+  selectizeInput(inputId=ns(id='feature_picker_gene_module'), label=NULL, choices=NULL, selected=NULL, multiple=FALSE) %>%
+    conditionalPanel(condition=sprintf('input["%s"]=="gene_modules"', ns(id='feature_type'))) -> gene_module_picker_conditional
+
 
   ## slider to limit colour range
   sliderInput(inputId=ns(id='value_range'), label='Colour range limits',
               min=0, max=1, step=0.1, value=c(-Inf,Inf)) -> value_range
 
+  ## checkbox for feature type
+  radioGroupButtons(inputId=ns(id='feature_type'), status='primary', width='100%',
+                    choices=list(`Features`='features', `Metadata`='metadata', `Gene modules`='gene_modules'),
+                    selected='features',
+                    checkIcon=list(yes=icon('ok', lib='glyphicon'))) -> feature_type_picker
+
   ## hidden text box to serve app
   textInput(inputId=ns('picked_feature'), label='picked feature') -> picked_feature_text_input
 
   # return ui element(s)
-  # tagList(dropdown, metadata_switch, value_range)
-  tagList(feature_names_picker,
-          if(include_metadata_switch) metadata_picker,
-          metadata_switch,
-          value_range,
+  tagList(h6('Feature selection'),
+          feature_type_picker,
+          feature_names_picker_conditional,
+          metadata_picker_conditional,
+          gene_module_picker_conditional,
+          if(include_values_range) value_range,
           hidden(picked_feature_text_input))
 }
 
@@ -74,13 +85,13 @@ feature_picker.server <- function(input, output, session, seurat, ...) {
   # get environments containing variables to run/configure this object
   collect_environments(id=parent.frame()$id, module='feature_picker') # provides `seuratvis_env`, `server_env` and `module_env`
   session_server <- get(x='session', env=server_env)
-  previously_picked_feature <- reactiveVal()
+  previously_picked_feature <- reactiveValues()
   tab <- parent.frame()$id
   seurat$picked_feature_values <- list()
 
   # react to the feature selection
   ## if a feature is selected, copy it to the reactive
-  observeEvent(eventExpr={input$feature_picker_feature_names }, handlerExpr={
+  observeEvent(eventExpr=input$feature_picker_feature_names, handlerExpr={
     # make sure these elements are defined
     req(input$feature_picker_feature_names)
     
@@ -88,7 +99,7 @@ feature_picker.server <- function(input, output, session, seurat, ...) {
     sprintf(fmt='### %sfeature_picker.server-observeEvent-input$feature_picker_feature_names [%s]', session$ns(''), input$feature_picker_feature_names) %>% message()
     
     # update hidden ui element
-    if(is.null(input$list_metadata) || !input$list_metadata)
+    if(input$feature_type=='features')
       updateTextInput(session=session, inputId='picked_feature', value=input$feature_picker_feature_names)})
 
   ## if a metadata column is selected, copy it to the reactive
@@ -100,21 +111,31 @@ feature_picker.server <- function(input, output, session, seurat, ...) {
     sprintf(fmt='### %sfeature_picker.server-observeEvent-input$feature_picker_metadata [%s]', session$ns(''), input$feature_picker_metadata) %>% message()
 
     # update hidden ui element
-    if(!is.null(input$list_metadata) && input$list_metadata)
+    if(input$feature_type=='metadata')
       updateTextInput(session=session, inputId='picked_feature', value=input$feature_picker_metadata)})
 
-  ## if the metadata switch is toggled, set the picked feature
-  observeEvent(eventExpr=input$list_metadata, ignoreInit=TRUE, handlerExpr={
+  ## if a gene module column is selected, copy it to the reactive
+  observeEvent(eventExpr=input$feature_picker_gene_module, handlerExpr={
     # make sure these elements are defined
+    req(input$feature_picker_gene_module)
 
     # send a message
-    sprintf(fmt='### %sfeature_picker.server-observeEvent-input$list_metadata [%s]', session$ns(''), input$list_metadata) %>% message()
+    sprintf(fmt='### %sfeature_picker.server-observeEvent-input$feature_picker_gene_module [%s]', session$ns(''), input$feature_picker_gene_module) %>% message()
+
+    # update hidden ui element
+    if(input$feature_type=='gene_modules')
+      updateTextInput(session=session, inputId='picked_feature', value=input$feature_picker_gene_module)})
+
+  ## update the hidden ui element when a feature type is selected
+  observeEvent(eventExpr=input$feature_type, ignoreInit=TRUE, handlerExpr={
+    # send a message
+    sprintf(fmt='### %sfeature_picker.server-observeEvent-input$feature_type [%s]', session$ns(''), input$feature_type) %>% message()
 
     # pick the feature to revert to
-    ## if metadata switch is true, get the value of the metadata dropdown
-    ## if metadata switch is false, get the previously shown feature (autocomplete_input return empty in this case)
-    picked_feature <- ifelse(input$list_metadata, input$feature_picker_metadata, previously_picked_feature())
-    previously_picked_feature(input$picked_feature)
+    input_name <- switch(input$feature_type, features='feature_picker_feature_names', metadata='feature_picker_metadata', gene_modules='feature_picker_gene_module')
+    picked_feature <- input[[input_name]]
+    if(input_name=='feature_picker_feature_names') # autocomplete_input can return empty in this case before it has been typed into
+      picked_feature <- previously_picked_feature[[tab]][[input$feature_type]]
 
     # update hidden ui element
     updateTextInput(session=session, inputId='picked_feature', value=picked_feature)})
@@ -123,16 +144,23 @@ feature_picker.server <- function(input, output, session, seurat, ...) {
   observeEvent(eventExpr=input$picked_feature, handlerExpr={
     # make sure these elements are defined
     req(seurat$object)
+    req(input$feature_type)
 
     # send a message
     sprintf(fmt='### %sfeature_picker.server-observeEvent-input$picked_feature [%s]', session$ns(''), input$picked_feature) %>% message()
 
     # create variables for shorthand
     picked <- input$picked_feature
-    object <- seurat$object
+
+    # track the history
+    previously_picked_feature[[tab]][[input$feature_type]] <- picked
 
     # get the values for the selected feature from the loaded Seurat
-    picked_feature_values <- FetchData(object=object, vars=picked) %>% set_names('value')
+    if(input$feature_type=='gene_modules') {
+      picked_feature_values <- seurat$gene_modules %>% select(picked) %>% set_names('value')
+    } else {
+      picked_feature_values <- FetchData(object=seurat$object, vars=picked) %>% set_names('value')
+    }
 
     # save feature information in the reactive
     #! TODO: this invalidates the features per cluster boxplot but not the umap
@@ -156,21 +184,22 @@ feature_picker.server <- function(input, output, session, seurat, ...) {
     # send a message
     sprintf(fmt='### %sfeature_picker.server-observeEvent-seurat$object [%s]', session$ns(''), seurat$formatted_project) %>% message()
 
-    # create variables for shorthand
-    object <- seurat$object
-
     # get the possible features and values
     ## get names of features and metadata
-    list(features=rownames(object),
-         metadata=colnames(object@meta.data)) -> feature_picker_options
+    list(features=rownames(seurat$object),
+         metadata=colnames(seurat$metadata),
+         gene_modules=colnames(seurat$gene_modules)) -> feature_picker_options
 
-    ## pick a random feature and metadata column
-    feature_picker_options %>% lapply(sample, size=1) -> feature_picker_selected
+    ## filter the options using the regex
+    feature_picker_options$features %<>% str_subset(pattern=regex(pattern=module_env$features_regex, ignore_case=TRUE))
+    feature_picker_options$metadata %<>% str_subset(pattern=regex(pattern=module_env$metadata_regex, ignore_case=TRUE))
 
-    # pick a feature to display: features or metadata
-    picked_feature <- feature_picker_selected$features
-    if(!is.null(input$list_metadata) && input$list_metadata)
-      picked_feature <- feature_picker_selected$metadata
+    ## pick a random feature and metadata and gene module column
+    feature_picker_options %>%
+      lapply(sample, size=1) -> feature_picker_selected
+
+    # pick a feature to display: features or metadata or gene_module
+    picked_feature <- feature_picker_selected %>% pluck(input$feature_type)
 
     # update the ui element(s)
     ## feature names autocomplete box
@@ -183,10 +212,15 @@ feature_picker.server <- function(input, output, session, seurat, ...) {
     updateSelectizeInput(session=session, inputId='feature_picker_metadata',
                          choices=feature_picker_options$metadata, selected=feature_picker_selected$metadata)
 
+    ## gene modules dropdown box
+    updateSelectizeInput(session=session, inputId='feature_picker_gene_module', choices='-spoof-')
+    updateSelectizeInput(session=session, inputId='feature_picker_gene_module',
+                         choices=feature_picker_options$gene_modules, selected=feature_picker_selected$gene_modules)
+
     ## hidden picked feature text input
     updateTextInput(session=session, inputId='picked_feature', value='-spoof-')
     updateTextInput(session=session, inputId='picked_feature', value=picked_feature)
 
     # update the reactive
-    previously_picked_feature(picked_feature)})
+    previously_picked_feature[[tab]] <- feature_picker_selected})
 }
