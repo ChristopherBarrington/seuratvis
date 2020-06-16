773c6b62d2a40c5d453b1b8db0253c029d4d770c
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-16 17:19:57 +0100

    Update ui_tab.gene_modules.R
    moved ui into tabpanels

--- R/ui_tab.gene_modules.R
+++ R/ui_tab.gene_modules.R
@@ -10,13 +10,7 @@
             fluidRow(project_name_text_box.ui(id=tab, width=8),
                      number_of_reads_text_box.ui(id=tab, width=2),
                      number_of_cells_text_box.ui(id=tab, width=2)),
-            fluidRow(column(width=9, gene_module_score_plot.ui(id=tab)),
-                     column(width=3, cluster_resolution_picker.ui(id=tab),
-                                     cluster_id_picker.ui(id=tab),
-                                     feature_picker.ui(id=tab, label='Gene module selection', include_features=FALSE, include_values_range=FALSE)
-                                     # add_gene_module.ui(id=tab),
-                                     # show_gene_module.ui(id=tab)
-                                     ))) -> content
+            fluidRow(gene_module_score_plot.ui(id=tab))) -> content
 
     # assign variables in the parent environment
     menuItem(text='Gene modules', tabName=NS(namespace=tab, id='tab'), icon=icon('layer-group'), badgeLabel='!', badgeColor='red') %>% list() %>% append(x=menus) -> menus
