267b79e556b98e20a1b2bc625fa05b07bc93b808
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-16 17:21:07 +0100

    Update plot.reduced_dimension_plot.R
    changed so each plot is independent - may move to independent servers to save the namespaces

--- R/plot.reduced_dimension_plot.R
+++ R/plot.reduced_dimension_plot.R
@@ -58,35 +58,39 @@ reduced_dimension_plot.server <- function(input, output, session, seurat, ...) {
   renderPlot(expr={
     # send a message
     session$ns('') %>% sprintf(fmt='### %sreduced_dimension_plot.server-renderPlot') %>% message()
+    seurat$feature_selector_refreshed # need this because `value_range_limits` and `picked_feature_values` upadted by the same observer so are isolated
 
     # because multiple plots are using the same `input` tag...
     #! TODO: could make each umap type a different module?? ie differnet ui id tags? in different servers perhaps?
     args <- list()
     args$opacity <- input_server[[session$ns('opacity_slider') %>% parse_ns_label()]]
     args$point_size <- input_server[[session$ns('point_size_slider') %>% parse_ns_label()]]
-    args$value_range_limits <- input_server[[session$ns('value_range') %>% parse_ns_label()]]
+    args$value_range_limits <- isolate(input_server[[session$ns('value_range') %>% parse_ns_label()]]) # isolate?
     args$cluster_id_picker <- input_server[[session$ns('cluster_id_picker') %>% parse_ns_label()]]
     args$label_clusters <- input_server[[session$ns('label_clusters') %>% parse_ns_label()]]
-    args$picked_feature_values <- isolate(seurat$picked_feature_values[[tab]])
+    args$picked_feature_values <- isolate(seurat$picked_feature_values[[tab]]) # isolate?
     args$dimred <- seurat$dimred[[tab]]
-    args$picked_cluster_resolution_idents <- seurat$picked_cluster_resolution_idents
+    args$picked_cluster_resolution_idents <- seurat$picked_cluster_resolution_idents[[tab]]
 
-    # make a base plot
-    cbind(args$dimred,
-          args$picked_cluster_resolution_idents,
-          {args$picked_feature_values %>% rename(picked_feature_value=value)}) %>%
-      mutate(is_selected_cluster_id=ident %in% args$cluster_id_picker) %>%
-      arrange(picked_feature_value) %>%
-      ggplot() +
-      aes(x=DIMRED_1, y=DIMRED_2) +
-      geom_hline(yintercept=0, colour='grey90') + geom_vline(xintercept=0, colour='grey90') +
-      geom_point(size=args$point_size, alpha=args$opacity) +
-      theme_void() +
-      theme(legend.position='none', legend.text=element_blank()) -> output_plot
+    args$opacity %<>% (function(x) ifelse(is.null(x), 1, x))
+    args$point_size %<>% (function(x) ifelse(is.null(x), 0.6, x))
 
     # get feature-specific plotting elements
     include_legend <- FALSE
     if(module_env$feature=='selected_cluster_resolution') {
+      # make a base plot
+      cbind(args$dimred,
+            args$picked_cluster_resolution_idents,
+            {args$picked_feature_values %>% rename(picked_feature_value=value)}) %>%
+        mutate(is_selected_cluster_id=ident %in% args$cluster_id_picker) %>%
+        arrange(picked_feature_value) %>%
+        ggplot() +
+        aes(x=DIMRED_1, y=DIMRED_2) +
+        geom_hline(yintercept=0, colour='grey90') + geom_vline(xintercept=0, colour='grey90') +
+        geom_point(size=args$point_size, alpha=args$opacity) +
+        theme_void() +
+        theme(legend.position='none', legend.text=element_blank()) -> output_plot
+
       # get reduced dimension coordinates and currently selected cluster resolution and make the scatterplot
       output_plot +
         aes(colour=ident) -> output_plot
@@ -105,6 +109,19 @@ reduced_dimension_plot.server <- function(input, output, session, seurat, ...) {
       }
       include_legend <- FALSE
     } else if(module_env$feature=='picked_feature_values') {
+      # make a base plot
+      cbind(args$dimred,
+            args$picked_cluster_resolution_idents,
+            {args$picked_feature_values %>% rename(picked_feature_value=value)}) %>%
+        mutate(is_selected_cluster_id=ident %in% args$cluster_id_picker) %>%
+        arrange(picked_feature_value) %>%
+        ggplot() +
+        aes(x=DIMRED_1, y=DIMRED_2) +
+        geom_hline(yintercept=0, colour='grey90') + geom_vline(xintercept=0, colour='grey90') +
+        geom_point(size=args$point_size, alpha=args$opacity) +
+        theme_void() +
+        theme(legend.position='none', legend.text=element_blank()) -> output_plot
+
       # if the picked feature has numeric values
       if(is.numeric(output_plot$data$picked_feature_value)) {
         # make the scatterplot
@@ -127,6 +144,19 @@ reduced_dimension_plot.server <- function(input, output, session, seurat, ...) {
         output_plot <- output_plot + colour_gradient
         include_legend <- TRUE
       } else { # the picked feature is factor-like
+        # make a base plot
+        cbind(args$dimred,
+              args$picked_cluster_resolution_idents,
+              {args$picked_feature_values %>% rename(picked_feature_value=value)}) %>%
+          mutate(is_selected_cluster_id=ident %in% args$cluster_id_picker) %>%
+          arrange(picked_feature_value) %>%
+          ggplot() +
+          aes(x=DIMRED_1, y=DIMRED_2) +
+          geom_hline(yintercept=0, colour='grey90') + geom_vline(xintercept=0, colour='grey90') +
+          geom_point(size=args$point_size, alpha=args$opacity) +
+          theme_void() +
+          theme(legend.position='none', legend.text=element_blank()) -> output_plot
+
         # make the scatterplot and facet by picked feature value
         output_plot +
           aes(colour=picked_feature_value) +
@@ -137,6 +167,18 @@ reduced_dimension_plot.server <- function(input, output, session, seurat, ...) {
         include_legend <- FALSE
       }
     } else if(module_env$feature=='selected_cluster_ids') {
+      # make a base plot
+      cbind(args$dimred,
+            args$picked_cluster_resolution_idents) %>%
+        mutate(is_selected_cluster_id=ident %in% args$cluster_id_picker) %>%
+        arrange(is_selected_cluster_id) %>%
+        ggplot() +
+        aes(x=DIMRED_1, y=DIMRED_2) +
+        geom_hline(yintercept=0, colour='grey90') + geom_vline(xintercept=0, colour='grey90') +
+        geom_point(size=args$point_size, alpha=args$opacity) +
+        theme_void() +
+        theme(legend.position='none', legend.text=element_blank()) -> output_plot
+
       output_plot +
         aes(colour=ident, alpha=is_selected_cluster_id) +
         scale_alpha_manual(values=c(`FALSE`=0.01, `TRUE`=1)) -> output_plot
