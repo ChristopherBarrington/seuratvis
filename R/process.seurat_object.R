a400b756eb12414f54f4ee818af6856f812e24bd
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-18 09:03:24 +0100

    Update process.seurat_object.R

--- R/process.seurat_object.R
+++ R/process.seurat_object.R
@@ -37,8 +37,8 @@ seurat_object.server <- function(input, output, session, seurat, ...) {
     # ensure we are using RNA assay and it is normalised
     selected_assay <- 'RNA'
     DefaultAssay(seurat) <- selected_assay
-    if(sum(seurat@assays[[selected_assay]]@counts)==sum(seurat@assays[[selected_assay]]@data))
-      seurat <- NormalizeData(seurat)
+    # if(sum(seurat@assays[[selected_assay]]@counts)==sum(seurat@assays[[selected_assay]]@data))
+    #   seurat <- NormalizeData(seurat)
 
     # update ui elements
     ## get the numeric metadata variables
