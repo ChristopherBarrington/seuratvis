eff44f2caca18a04562cf6df7198745709cf420d
Christopher Barrington <christopher.barrington@crick.ac.uk>
2020-06-29 16:33:05 +0100

    added ui elements to cluster filter tab

--- R/tab.preprocess.r
+++ R/tab.preprocess.r
@@ -12,8 +12,7 @@ preprocessing.tab <- function() {
     # define the content of each tab
     ## filter cells by per-cell metrics
     tab_name <- 'cell_filtering_tab'
-    tabItem(tabName=tab_name,
-            h1('Identify cells that can be removed with quality filters'),
+    tabItem(tabName=tab_name, h1('Identify cells that can be removed with quality filters'),
             fluidRow(dataset_info_text_box.ui(id=NS(tab_name, 'project_name'), width=12),
                      dataset_info_text_box.ui(id=NS(tab_name, 'n_umi'), width=3),
                      dataset_info_text_box.ui(id=NS(tab_name, 'n_cells'), width=3),
@@ -33,8 +32,7 @@ preprocessing.tab <- function() {
                              column(width=4, offset=1, density.plot(id=NS(tab_name, 'proportion_mt')))))) -> cell_filtering_tab
     ## dimensionality tab
     tab_name <- 'dimensionality_tab'
-    tabItem(tabName='dimensionality_tab',
-            h1('Determine the dimensionality of a dataset'),
+    tabItem(tabName='dimensionality_tab', h1('Determine the dimensionality of a dataset'),
             fluidRow(dataset_info_text_box.ui(id=NS('dimensionality_tab', 'project_name'), width=12)),
             fluidRow(boxPlus(title='Elbow plot', closable=FALSE, width=6, status='primary', dimensionality.plot(id=NS('dimensionality_tab','elbow'))),
                      boxPlus(title='Top contributing features', closable=FALSE, width=6, status='primary', dimensionality.plot(id=NS('dimensionality_tab','top_features'))),
@@ -42,10 +40,19 @@ preprocessing.tab <- function() {
                      boxPlus(title='JackStraw p-values', closable=FALSE, width=6, status='primary', dimensionality.plot(id=NS('dimensionality_tab','jackstraw_pvalue'))))) -> dimensionality_tab
     
     ## filter clusters using cluster-wide metrics
-    tab_name <- 'cluster_filtering_tab'
-    tabItem(tabName=tab_name,
-            h1('Remove clusters of cells before reanalysis'),
-            p('Use these tools to identify clusters from the reduced dimension map that could be removed. Clusters that represent contaminant or are characterised by high proportion of mitochondrial RNA, for example, may be suitable for removal before the remaining cell set is re-analysed.')) -> cluster_filtering_tab
+    tab_name <- ns <- 'cluster_filtering_tab'
+    ns %<>% NS()
+    tabItem(tabName=tab_name, h1('Remove clusters of cells before reanalysis'),
+            p('Use these tools to identify clusters from the reduced dimension map that could be removed. Clusters that represent contaminant or are characterised by high proportion of mitochondrial RNA, for example, may be suitable for removal before the remaining cell set is re-analysed.'),
+            fluidRow(dataset_info_text_box.ui(id=ns('project_name'), width=7),
+                     picked_feature_and_description_text_box.ui(id=ns('feature_description'), width=5),
+                     dataset_info_text_box.ui(id=ns('n_umi'), width=3),
+                     dataset_info_text_box.ui(id=ns('n_cells'), width=3),
+                     dataset_info_text_box.ui(id=ns('n_umi_per_cell'), width=3),
+                     dataset_info_text_box.ui(id=ns('n_features_per_cell'), width=3)),
+            fluidRow(boxPlus(title='Selected feature', closable=FALSE, width=4, dimension_reduction.plot(id=ns('picked_feature'))),
+                     boxPlus(title='Selected cluster(s)', closable=FALSE, width=4, dimension_reduction.plot(id=ns('picked_clusters'))),
+                     boxPlus(title='Feature values in clusters', closable=FALSE, width=4, feature_value_per_cluster.plot(id=ns('feature_value_per_cluster'))))) -> cluster_filtering_tab
 
     # collect all of the contents for this tab group
     content <- list(cell_filtering_tab, dimensionality_tab, cluster_filtering_tab)
@@ -119,4 +126,52 @@ dimensionality_tab.server <- function(input, output, session, server_input, serv
   callModule(module=dimensionality.jackstraw, id='jackstraw', seurat=seurat, picked_reduction=reduction_picker, picked_components=components_picker)
   callModule(module=dimensionality.jackstraw_pvalue, id='jackstraw_pvalue', seurat=seurat, picked_reduction=reduction_picker)
   callModule(module=dimensionality.top_features_pca_heatmap, id='top_features', seurat=seurat, picked_reduction=reduction_picker, picked_components=components_picker)
+}
+
+#'
+#' 
+cluster_filtering_tab.server <- function(input, output, session, server_input, server_output, server_session, seurat) {
+  tab <- session$ns('') %>% str_remove('-$')
+  
+  # build the sidebar ui
+  observeEvent(eventExpr=server_input$left_sidebar, handlerExpr={
+    if(server_input$left_sidebar==tab) {    
+      tab %<>% str_c('-')
+      renderUI({tagList(filter_n_umi.ui(id=tab, seurat=seurat),
+                        filter_n_features.ui(id=tab, seurat=seurat),
+                        filter_proportion_mt.ui(id=tab, seurat=seurat),
+                        dimension_reduction.ui(id=tab, seurat=seurat),
+                        cluster_picker.ui(id=tab, seurat=seurat, resolution=TRUE, picker=FALSE, label_switch=TRUE),
+                        feature_picker.ui(id=tab, seurat=seurat),
+                        show_filtering_parameters.ui(id=tab, label='Cell filtering parameters'))})  -> server_output$right_sidebar.data_opts
+      renderUI({tagList()}) -> server_output$right_sidebar.plotting_opts}})
+
+  # call the modules for this tab
+  callModule(module=filter_n_umi.server, id='', cell_filtering=filtering_parameters)
+  # callModule(module=filter_n_features.server, id='', cell_filtering=filtering_parameters)
+  callModule(module=filter_proportion_mt.server, id='', cell_filtering=filtering_parameters)
+
+  dimension_reduction <- callModule(module=dimension_reduction.server, id='', seurat=seurat, regex='.*')
+  cluster_resolution <- callModule(module=cluster_picker.server, id='', seurat=seurat)
+  feature_picker <- callModule(module=feature_picker.server, id='', seurat=seurat)
+  filtering_parameters <- callModule(module=dataset_filtering.server, id='filtering', seurat=seurat)
+  # point_size <- callModule(module=point_size.server, id='')
+  # opacity <- callModule(module=opacity.server, id='')
+  colour_picker <- list(low='linen', mid='white', high='darkviolet', background=rgb(255, 255, 255, 255, max=255))
+  point_size <- list(size=0.6)
+  opacity <- list(alpha=1)
+
+  callModule(module=dataset_info_text_box.project_name, id='project_name', seurat=seurat)
+  callModule(module=picked_feature_and_description_text_box.server, id='feature_description', seurat=seurat, picked_feature=feature_picker)
+  callModule(module=dataset_info_text_box.n_filtered_umi, id='n_umi', seurat=seurat, cell_filtering=filtering_parameters)
+  callModule(module=dataset_info_text_box.n_filtered_cells, id='n_cells', seurat=seurat, cell_filtering=filtering_parameters)
+  callModule(module=dataset_info_text_box.n_umi_per_filtered_cell, id='n_umi_per_cell', seurat=seurat, cell_filtering=filtering_parameters)
+  callModule(module=dataset_info_text_box.n_features_per_filtered_cell, id='n_features_per_cell', seurat=seurat, cell_filtering=filtering_parameters)
+
+  callModule(module=show_filtering_parameters.server, id='', seurat=seurat, cell_filtering=filtering_parameters)
+
+  callModule(module=dimension_reduction.highlight_feature.server, id='picked_feature', dimension_reduction=dimension_reduction, picked_feature=feature_picker, picked_colours=colour_picker, opacity=opacity, point_size=point_size)
+  callModule(module=dimension_reduction.show_selected_clusters.server, id='picked_clusters', dimension_reduction=dimension_reduction, picked_colours=colour_picker, point_size=point_size, cluster_resolution=cluster_resolution)
+  callModule(module=feature_value_per_cluster.server, id='feature_value_per_cluster', picked_feature=feature_picker, cluster_resolution=cluster_resolution, picked_colours=colour_picker)
+
 }
